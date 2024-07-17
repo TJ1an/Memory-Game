1a0b670eef2cfefd0d0e636770d81afc12058bfb
Ian Leong <ianleong726@gmail.com>
2024-07-17 22:15:51 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://tj1an.github.io/Memory-Game/",
+  base: "/Memory-Game/",
   plugins: [react()],
 });
