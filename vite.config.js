a1b1c00beaeb4fea2c38197adf28297e23eea135
Ian Leong <ianleong726@gmail.com>
2024-07-17 22:24:36 +0800

    Revert "update vite config"
    This reverts commit 1a0b670eef2cfefd0d0e636770d81afc12058bfb.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Memory-Game/",
+  base: "https://tj1an.github.io/Memory-Game/",
   plugins: [react()],
 });
